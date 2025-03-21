34ff84069d0317a68fc0279a6d455df2c940c352
Tomiri-sudo <tomiriexpt@gmail.com>
2025-03-21 06:48:10 +0100

    just another library update

--- src/graphics.c
+++ src/graphics.c
@@ -1,7 +1,7 @@
 #include "../include/graphics.h"
 #include "../include/SDL2/SDL.h"
 #include "../include/ctype.h"
-#include <string.h>
+#include "../include/string.h"
 
 // Global SDL variables.
 static SDL_Window* window;
@@ -13,37 +13,6 @@ typedef struct {
     int num_segments;
     int segments[10][4];  // Each segment: x1, y1, x2, y2.
 } Letter;
-
-int main() {
-    init_graphics(800, 600);
-
-    // Set white background.
-    set_background_color(255, 255, 255);
-
-    // Set drawing color to red for shapes.
-    set_color(255, 0, 0);
-    draw_line(100, 100, 300, 300);
-    draw_rect(400, 200, 100, 50);
-
-    // Set drawing color to blue for text.
-    set_color(0, 0, 255);
-    draw_text(50, 50, "AB BA");
-
-    present();
-
-    SDL_Event event;
-    int running = 1;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT)
-                running = 0;
-        }
-        SDL_Delay(16);  // Roughly 60 FPS.
-    }
-
-    cleanup_graphics();
-    return 0;
-}
 
 // Only a couple of letters for demonstration purposes.
 static Letter letters[] = {
